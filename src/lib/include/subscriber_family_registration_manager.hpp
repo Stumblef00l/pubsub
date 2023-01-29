97824de77c4bb16a9541d7261b0326f28f16038d
Stumblef00l <kunalbonucci@gmail.com>
2023-01-29 13:24:04 +0530

    Add ISubscriberFamilyRegistrationManager interface

--- src/lib/include/subscriber_family_registration_manager.hpp
+++ src/lib/include/subscriber_family_registration_manager.hpp
@@ -1,6 +1,18 @@
 #ifndef _SUBSCRIBER_FAMILY_REGISTRATION_MANAGER_HPP_
 #define _SUBSCRIBER_FAMILY_REGISTRATION_MANAGER_HPP_
 
-class ISubscriberFamilyRegistrationManager {};
+#include <memory>
+#include <vector>
+#include "subscriber.hpp"
+
+class ISubscriberFamilyRegistrationManager {
+    public:
+        virtual void registerSubscriber(ISubscriber* subscriber) = 0;
+        virtual void unregisterSubscriber(ISubscriber* subscriber) = 0;
+        virtual std::vector<ISubscriber*> getSubscribers() = 0;
+
+    protected:
+        std::vector<ISubscriber*> subscribers_;
+};
 
 #endif
